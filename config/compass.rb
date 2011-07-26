b84a5358aefa0da0120c478c4e84224fb690ac00
Jeff Mehlhoff <jeffmehlhoff@mac.com>
2011-07-25 21:40:57 -0400

    Trying to make Heroku + Compass become friends.
    Signed-off-by: Jeff Mehlhoff <jeffmehlhoff@mac.com>

--- config/compass.rb
+++ config/compass.rb
@@ -1,24 +1,37 @@
+
+# This configuration file works with both the Compass command line tool and within Rails.
+# Require any additional compass plugins here.
+project_type = :rails
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+
+
+# Heroku...
 unless Rails.env.production?
-  # This configuration file works with both the Compass command line tool and within Rails.
-  # Require any additional compass plugins here.
-  project_type = :rails
+  require 'fileutils'
+  FileUtils.mkdir_p(Rails.root.join("tmp", "stylesheets"))
 
-  # Set this to the root of your project when deployed:
-  http_path = "/"
-
-  # You can select your preferred output style here (can be overridden via the command line):
-  output_style = :compressed
-
-  # To enable relative paths to assets via compass helper functions. Uncomment:
-  # relative_assets = true
-
-  # To disable debugging comments that display the original location of your selectors. Uncomment:
-  # line_comments = false
-
-
-  # If you prefer the indented syntax, you might want to regenerate this
-  # project again passing --syntax sass, or you can uncomment this:
-  # preferred_syntax = :sass
-  # and then run:
-  # sass-convert -R --from scss --to sass app/stylesheets scss && rm -rf sass && mv scss sass
+  Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+      :urls => ['/stylesheets'],
+      :root => "#{Rails.root}/tmp")
+  
+  css_dir = 'tmp/stylesheets'
+  
 end
